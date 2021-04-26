74069befbf5b5d3e12c0950215c9c6f90dfd99ec
Xavier Tan <MetatronEX@users.noreply.github.com>
2021-04-26 21:00:00 +0800

    commit source

---
+++ sandbox/include/vk_sandbox.hpp
@@ -0,0 +1,39 @@
+#define VK_USE_PLATFORM_WIN32_KHR
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>
+
+#include <stdexcept>
+#include <cstdlib>
+#include <iostream>
+
+namespace sandbox
+{
+	class sandbox_app
+	{
+	public:
+
+		static constexpr char* app_name = "sandbox";
+
+		void run();
+
+	private:
+
+		static constexpr unsigned RES_WIDTH = 1280;
+		static constexpr unsigned RES_HEIGHT = 720;
+
+		void initialize();
+		void app_loop();
+		void cleanup();
+
+		GLFWwindow*					window;
+		VkInstance					instance;
+		VkPhysicalDevice			phy_dev		{ VK_NULL_HANDLE };
+		VkDevice					dev;
+		VkSurfaceKHR				surface;
+
+		VkQueue						graphics_queue;
+		VkQueue						present_queue;
+	};
+}
