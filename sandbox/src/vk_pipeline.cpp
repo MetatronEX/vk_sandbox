4a0fa50e052605d6be1ebfaea3e1a8ac61e5493b
xtan <xtan@virtuosgames.com>
2021-08-10 10:55:58 +0800

    mid save

---
+++ sandbox/src/vk_pipeline.cpp
@@ -0,0 +1,54 @@
+#include "vk_pipeline.hpp"
+
+namespace vk
+{
+	void pipeline::setup_queried_features()
+	{
+
+	}
+
+	void pipeline::prime()
+	{
+
+	}
+
+	void pipeline::setup_depth_stencil()
+	{
+
+	}
+
+	void pipeline::setup_drawcommands()
+	{
+
+	}
+
+	void pipeline::setup_renderpass()
+	{
+
+	}
+
+	void pipeline::setup_framebuffers()
+	{
+
+	}
+
+	void pipeline::prapare()
+	{
+
+	}
+
+	void pipeline::update()
+	{
+
+	}
+
+	void pipeline::render()
+	{
+
+	}
+
+	void pipeline::cleanup()
+	{
+
+	}
+}
